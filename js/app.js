4fc48fde020facf8aed14fb1b7e9ef23c2b0c7b8
d3n2dboy <d3n2dboy@gmail.com>
2025-04-07 22:28:41 +0500

    Updates

--- js/app.js
+++ js/app.js
@@ -1127,7 +1127,6 @@
                             spollerTitle.classList.toggle("_spoller-active");
                             _slideToggle(spollerTitle.nextElementSibling, spollerSpeed);
                         }
-                        e.preventDefault();
                     }
                 }
                 function hideSpollersBody(spollersBlock) {
@@ -4255,6 +4254,42 @@
             use_native: true
         });
         lazyMedia.update();
+        let addWindowScrollEvent = false;
+        function headerScroll() {
+            addWindowScrollEvent = true;
+            const header = document.querySelector("header.header");
+            console.log(header);
+            const headerShow = header.hasAttribute("data-scroll-show");
+            const headerShowTimer = header.dataset.scrollShow ? header.dataset.scrollShow : 500;
+            const startPoint = header.dataset.scroll ? header.dataset.scroll : 1;
+            let scrollDirection = 0;
+            let timer;
+            document.addEventListener("windowScroll", (function(e) {
+                const scrollTop = window.scrollY;
+                clearTimeout(timer);
+                if (scrollTop >= startPoint) {
+                    !header.classList.contains("_header-scroll") ? header.classList.add("_header-scroll") : null;
+                    if (headerShow) {
+                        if (scrollTop > scrollDirection) header.classList.contains("_header-show") ? header.classList.remove("_header-show") : null; else !header.classList.contains("_header-show") ? header.classList.add("_header-show") : null;
+                        timer = setTimeout((() => {
+                            !header.classList.contains("_header-show") ? header.classList.add("_header-show") : null;
+                        }), headerShowTimer);
+                    }
+                } else {
+                    header.classList.contains("_header-scroll") ? header.classList.remove("_header-scroll") : null;
+                    if (headerShow) header.classList.contains("_header-show") ? header.classList.remove("_header-show") : null;
+                }
+                scrollDirection = scrollTop <= 0 ? 0 : scrollTop;
+            }));
+        }
+        setTimeout((() => {
+            if (addWindowScrollEvent) {
+                let windowScroll = new Event("windowScroll");
+                window.addEventListener("scroll", (function(e) {
+                    document.dispatchEvent(windowScroll);
+                }));
+            }
+        }), 0);
         /*!
  * lightgallery | 2.7.1 | January 11th 2023
  * http://www.lightgalleryjs.com/
@@ -5996,5 +6031,6 @@ PERFORMANCE OF THIS SOFTWARE.
         isWebp();
         menuInit();
         spollers();
+        headerScroll();
     })();
 })();
